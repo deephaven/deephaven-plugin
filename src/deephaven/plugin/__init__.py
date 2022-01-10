786d7ac1d353249e0b0c0109cca5a4a18d46593e
Devin Smith <devinsmith@deephaven.io>
2022-01-10 14:49:21 -0800

    Bump version, add release step

--- src/deephaven/plugin/__init__.py
+++ src/deephaven/plugin/__init__.py
@@ -1,7 +1,7 @@
 import abc
 from typing import Union, Type
 
-__version__ = '0.0.1.dev0'
+__version__ = '0.0.1.dev1'
 
 DEEPHAVEN_PLUGIN_ENTRY_KEY = 'deephaven.plugin'
 DEEPHAVEN_PLUGIN_REGISTRATION_CLASS = 'registration_cls'
