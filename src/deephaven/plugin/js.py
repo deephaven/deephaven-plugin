ad7b05d9d603f8c991daaedd95f50208cb317405
Devin Smith <devinsmith@deephaven.io>
2022-09-26 09:15:41 -0700

    Add documentation

--- src/deephaven/plugin/js.py
+++ src/deephaven/plugin/js.py
@@ -1,4 +1,5 @@
 import abc
+import json
 import pathlib
 import typing
 
@@ -6,6 +7,31 @@ from . import Plugin
 
 
 class JsType(Plugin):
+    """A javascript type plugin. Useful for adding custom javascript code to the server."""
+
     @abc.abstractmethod
     def path(self) -> typing.Generator[pathlib.Path, None, None]:
+        """A generator that yields the path to package.json."""
         pass
+
+    def package_dict(self) -> dict:
+        """Returns package.json as a dict"""
+        with self.path() as path:
+            with open(path) as file:
+                return json.load(file) 
+
+    @property
+    def name(self) -> str:
+        """Returns the name field from package.json"""
+        return self.package_dict()['name']
+
+    @property
+    def version(self) -> str:
+        """Returns the version field from package.json"""
+        return self.package_dict()['version']
+
+    def __str__(self) -> str:
+        pd = self.package_dict()
+        name = pd['name']
+        version = pd['version']
+        return f"{name}@{version}"
--- src/deephaven/plugin/object.py
+++ src/deephaven/plugin/object.py
@@ -5,40 +5,53 @@ from . import Plugin, Registration, register_all_into
 
 
 class Reference:
+    """A reference."""
     def __init__(self, index: int, type: Optional[str]):
         self._index = index
         self._type = type
 
     @property
     def index(self) -> int:
+        """The index, which is defined by the order in which references are created.
+        May be used in the output stream to refer to the reference from the client."""
         return self._index
 
     @property
     def type(self) -> Optional[str]:
+        """The type."""
         return self._type
 
 
 class Exporter(abc.ABC):
+    """The interface for creating new references during ObjectType.to_bytes."""
     @abc.abstractmethod
     def reference(self,
                   object,
                   allow_unknown_type: bool = False,
                   force_new: bool = False) -> Optional[Reference]:
+        """Gets the reference for object if it has already been created and force_new is False,
+        otherwise creates a new one. If allow_unknown_type is False, and no type can be found, no
+        reference will be created."""
         pass
 
 
 class ObjectType(Plugin):
+    """An object type plugin. Useful for serializing custom objects between the server / client."""
+    
     @property
     @abc.abstractmethod
     def name(self) -> str:
+        """The name of the object type."""
         pass
 
     @abc.abstractmethod
     def is_type(self, object) -> bool:
+        """Returns true if, and only if, the object is compatible with this object type."""
         pass
 
     @abc.abstractmethod
     def to_bytes(self, exporter: Exporter, object) -> bytes:
+        """Serializes object into bytes. Must only be called with a compatible object."""
         pass
 
 
